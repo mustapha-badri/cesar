aef498dfc856b30897dba27f05ecbaf6544ee7de
hamzaaitpython <76910557+hamzaaitpython@users.noreply.github.com>
2023-01-01 16:10:52 +0100

    Add files via upload

--- algorithme_cesar.py
+++ algorithme_cesar.py
@@ -1,18 +1,20 @@
 import sys 
 
 
+# Demander à l'utilisateur le mot et le décalage.
 mot = input("Saisissez une phrase ou un mot : ")
 mot_decale = ""
 decalage = input("Saisissez un décalage : ")
 
 
+# Arrêter le programme si le décalage est invalide.
 if decalage.isnumeric() == False:
     print("Le décalage est invalide.")
     sys.exit()
 
 
+# Initialisation du dictionnaire.
 valeur_decalage = {
-
     "a" : 0,
     "b" : 1,
     "c" : 2,
@@ -75,23 +77,42 @@ valeur_decalage = {
     "X" : 59,
     "Y" : 60,
     "Z" : 61,
-
+    "(" : 62,
+    ")" : 63,
+    "*" : 64,
+    " " : 65,
+    "+" : 66,
+    "-" : 67,
+    "_" : 68,
+    "#" : 69,
+    "{" : 70,
+    "}" : 71,
+    "[" : 72,
+    "]" : 73,
+    "/" : 74,
+    ":" : 75,
 }
 
-
+# Séparer les lettres du mot dans une liste.
 lettres_separees = list(mot)
 
+# Parcourir la liste.
 for i in range(len(lettres_separees)):
+    # Vérifier que la lettre est dans le dictionnaire.
     if lettres_separees[i] in valeur_decalage:
+        # Parcourir le dictionnaire et stocker la valeur de la lettre qu'il doit remplacer celle-ci.
         for x in valeur_decalage:
             if x == lettres_separees[i]:
                 valeur = (valeur_decalage[x] + int(decalage)) % len(valeur_decalage)
                 break
+        # Reparcourir le dictionnaire et faire le remplacement.
         for y in valeur_decalage:
             if valeur_decalage[y] == valeur:
                 lettres_separees[i] = y
 
+# Retransformer la liste en chaine de caractères.
 mot_decale = "".join(lettres_separees)
 
+# Afficher le mot décalé.
 print(mot_decale)
 
